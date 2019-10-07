1c50d405071d097985db8837e52984e8c63e74cf
gonzalormonja <gonzalormonja1@gmail.com>
2019-10-07 13:02:23 -0300

    s

--- TpPython/TpPythonProject/TpPythonApp/models.py
+++ TpPython/TpPythonProject/TpPythonApp/models.py
@@ -3,9 +3,25 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-class Producto(models.Model):
-    nombreProducto = models.CharField(max_length=255)
-    cantidadProducto = models.IntegerField(default=0)
-    precioProducto = models.FloatField()
-    categoriaProducto = models.CharField(max_length=255)
 
+
+
+
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=255)
+    categoriaPadre = models.ForeignKey('self',null=True,blank=True)
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=255)
+    cantidad = models.IntegerField(default=0)
+    categoria = models.ForeignKey('Categoria')
+
+class Precio(models.Model):
+    precio = models.FloatField()
+    fechaPrecio = models.DateField()
+    producto = models.ForeignKey('Producto',related_name='precios')
+
+class Empresa(models.Model):
+    codigo = models.CharField(max_length=255)
+    categorias = models.ManyToManyField('Categoria')
