7d87bf21c50c9d5b434c587c6bb783a2ce2884d9
gonzalormonja <gonzalormonja1@gmail.com>
2019-10-22 15:44:44 -0300

    t

--- TpPython/TpPythonProject/TpPythonApp/serializers.py
+++ TpPython/TpPythonProject/TpPythonApp/serializers.py
@@ -1,8 +1,24 @@
+#archivo creado para serializar.
+from rest_framework import serializers
+from .models import Producto,Precio,Categoria,Empresa
+
+
+class PrecioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Precio
+        fields = ('id','precio','fechaPrecio')
 
 
 
 
+class CategoriaParaProductoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Categoria
+        fields=('id','nombre')
 
-
-
-
+class ProductoSerializer(serializers.ModelSerializer):
+    precios = PrecioSerializer(many=True)
+    categoria_set = serializers.ReadOnlyField(source='categoria.nombre')
+    class Meta:
+        model = Producto
+        fields = ('id','nombre','cantidad','categoria_set','precios')
